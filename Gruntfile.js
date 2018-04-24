74a81f7ad59352a5968b5e8da4bee181d3e45c56
张永兴 <yongxing.zhang@hand-china.com>
2018-04-24 15:15:41 +0800

    [IMP] 添加文档搜索功能

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,80 +4,88 @@ var Entities = require('html-entities').AllHtmlEntities;
 var Html = new Entities();
 var marked = require("marked");
 
-var CONTENT_PATH_PREFIX = "./content/blog";
-var documentsStore = {};
-
 module.exports = function(grunt) {
 
     grunt.registerTask("index", function() {
+		var generateFiles = function(documentsStore,CONTENT_PATH_PREFIX,filename) {
+			var indexPages = function() {
+				var pagesIndex = [];
+				grunt.file.recurse(CONTENT_PATH_PREFIX, function(abspath, rootdir, subdir, filename) {
+					grunt.verbose.writeln("Parse file:",abspath);
+					var temp=processFile(abspath, filename);
+					if(temp){
+						pagesIndex.push(temp);
+					}
+				});
 		
-		 var indexPages = function() {
-            var pagesIndex = [];
-            grunt.file.recurse(CONTENT_PATH_PREFIX, function(abspath, rootdir, subdir, filename) {
-                grunt.verbose.writeln("Parse file:",abspath);
-                var temp=processFile(abspath, filename);
-                if(temp){
-                    pagesIndex.push(temp);
-                }
-            });
-
-            return documentsStore;
-        };
-		
-		var processFile = function(abspath, filename) {
-            var pageIndex;
-
-            if (S(filename).endsWith(".md")){
-                pageIndex = processMDFile(abspath, filename);
-            } else {
-                return null;
-            }
-            return pageIndex;
-        };
-        
-		var processMDFile = function(abspath, filename) {
-			var body = grunt.file.read(abspath);
-
-			if(!body){
-				return null;
-			}
-			body = body.split("+++");
-			var frontMatter;
-			try {
-				frontMatter = toml.parse(body[1].trim());
-			} catch (e) {
-				console.log(e.message);
-				return null;
-			}
-			
-			var href = S(abspath).chompLeft("content").chompRight(".md").s;
-			if (filename === "index.md") {
-				href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(filename).s;
-			}
-
-			var content = body[2].trim();
-			content = marked(content);
-			content = content.replace(/\<script.*\/script\>/g,"");
-			content = Html.decode(content);
-			content = content.replace(/(<([^>]+)>)/ig, '');
-			content = content.replace(/[\n,\r ]+/g, ' ');
-			
-			var doc = {
-				url: href.slice(1),
-                title: frontMatter.title,
-                date: frontMatter.date,
-                author: frontMatter.author,
-                description: frontMatter.description,
-                img: frontMatter.img,
-                tags: frontMatter.tags,
-                categories: frontMatter.categories,
-				body: content
+				return documentsStore;
 			};
-
-			documentsStore[href] = doc;
-		};
+			
+			var processFile = function(abspath, filename) {
+				var pageIndex;
 		
-		grunt.file.write("./static/lunr.json", JSON.stringify(indexPages()));
-        grunt.log.ok("Index built");
+				if (S(filename).endsWith(".md")){
+					pageIndex = processMDFile(abspath, filename);
+				} else {
+					return null;
+				}
+				return pageIndex;
+			};
+			
+			var processMDFile = function(abspath, filename) {
+				var body = grunt.file.read(abspath);
+		
+				if(!body){
+					return null;
+				}
+				body = body.split("+++");
+				var frontMatter;
+				try {
+					frontMatter = toml.parse(body[1].trim());
+				} catch (e) {
+					console.log(e.message);
+					return null;
+				}
+				
+				var href = S(abspath).chompLeft("content").chompRight(".md").s;
+				if (filename === "index.md") {
+					href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(filename).s;
+				}
+		
+				var content = body[2].trim();
+				content = marked(content);
+				content = content.replace(/\<script.*\/script\>/g,"");
+				content = Html.decode(content);
+				content = content.replace(/(<([^>]+)>)/ig, '');
+				content = content.replace(/[\n,\r ]+/g, ' ');
+				
+				var entity = {
+					url: href.slice(1),
+					title: frontMatter.title,
+					date: frontMatter.date,
+					author: frontMatter.author,
+					description: frontMatter.description,
+					img: frontMatter.img,
+					tags: frontMatter.tags,
+					categories: frontMatter.categories
+					
+				};
+
+				entity.body=content;
+				documentsStore[href] = entity;
+			};
+			
+			grunt.file.write(filename, JSON.stringify(indexPages()));
+			grunt.log.ok("Index built");
+		}
+		var BLOGS_PATH_PREFIX = "./content/blog";
+		var DOCS_PATH_PREFIX = "./content/docs";
+		var blogsStore = {};
+		var docsStore = {};
+		var blogfilename = "./static/lunr.json";
+		var docsfilename = "./static/docslunr.json";
+		generateFiles(blogsStore,BLOGS_PATH_PREFIX,blogfilename);
+		generateFiles(docsStore,DOCS_PATH_PREFIX,docsfilename);
+		
     });
 };
