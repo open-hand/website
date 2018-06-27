3eff913fb4224bece72c839f5891427ed9b744f9
yooxinz <zhangyongxing89@gmail.com>
2018-06-27 09:34:39 +0800

    [IMP] update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,15 @@
 // compress js
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-gulp.task('jscompress', function() {
-   return gulp.src('themes/choerodon-document-theme/static/src/js/*')
-        .pipe(uglify())
-        .pipe(gulp.dest('themes/choerodon-document-theme/static/js'));
-});
-
 // compress css
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('csscompress', function() {
-    // 1. 找到文件
-  return  gulp.src('themes/choerodon-document-theme/static/src/css/*')
-    // 2. 压缩文件
-        .pipe(cleanCSS())
-        // 3. 另存压缩后的文件
-        .pipe(gulp.dest('themes/choerodon-document-theme/static/css'));
+gulp.task('default', function(){
+  gulp.src('themes/choerodon-document-theme/static/css/*')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('themes/choerodon-document-theme/static/css'));
+      
+  gulp.src('themes/choerodon-document-theme/static/js/*')
+      .pipe(uglify())
+      .pipe(gulp.dest('themes/choerodon-document-theme/static/js'));
 });
