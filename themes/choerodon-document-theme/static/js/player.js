99d9b8781aa0e4a5463dca4c3de32710bd0043e3
ale <ale0720@163.com>
2019-07-10 22:36:26 +0800

    [IMP]迁移知识分享视频到哔哩哔哩

--- themes/choerodon-document-theme/static/js/player.js
+++ themes/choerodon-document-theme/static/js/player.js
@@ -1,20 +1,27 @@
-jQuery(document).ready(function(){
-	//教程播放
-    $('#tutorial-img .tutorial,.footer-right i').on('click', function (e) {
-        if (isPC){
-            var src = "https://v.qq.com/txp/iframe/player.html?vid="+ $(this).attr('data-src') +"&tiny=0&auto=1";
-            $('#tutorial-video iframe').attr("src", src);
-            $('#tutorial-video').css("display", "flex");
-        } else {
-            var src = "https://v.qq.com/x/page/" + $(this).attr('data-src')+".html";
-            window.location.href=src;
-        }
-    })
-    $('#tutorial-video').on('click', function (e) {
-        if ($(e.target).hasClass("bg") || $(e.target).hasClass("icon-guanbi")) {
-            $('#tutorial-video').css("display", "none");
-            $('#tutorial-video iframe').attr("src", "");
-        }
-    })
-	
-})
+jQuery(document).ready(function () {
+  //教程播放
+
+  console.log('player');
+  $('#tutorial-img .tutorial,.footer-right i').on('click', function (e) {
+    if (isPC) {
+      var src =
+        '//player.bilibili.com/player.html?aid=' +
+        $(this).attr('data-aid') +
+        '&cid=' +
+        $(this).attr('data-cid') +
+        '&page=1';
+      $('#tutorial-video iframe').attr('src', src);
+      $('#tutorial-video').css('display', 'flex');
+    } else {
+      var src = 'https://www.bilibili.com/video/' + $(this).attr('data-src');
+      window.location.href = src;
+    }
+  });
+  $('#tutorial-video').on('click', function (e) {
+    if ($(e.target).hasClass('bg') || $(e.target).hasClass('icon-guanbi')) {
+      $('#tutorial-video').css('display', 'none');
+      $('#tutorial-video iframe').attr('src', '');
+    }
+  });
+
+});
