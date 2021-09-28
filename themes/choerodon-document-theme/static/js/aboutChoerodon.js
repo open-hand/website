eddbcfdcfeb84190f80d25fd69ef9ee6df1f19c3
20399 <yanfang.lin@hand-china.com>
2021-09-28 16:45:55 +0800

    [FIX] 修复导航栏hover显示子菜单失效问题

--- themes/choerodon-document-theme/static/js/aboutChoerodon.js
+++ themes/choerodon-document-theme/static/js/aboutChoerodon.js
@@ -1,43 +1,40 @@
-jQuery(document).ready(function () {
-
-  function numAutoPlus(targetEle) {
-    // 动画时长(毫秒数)
-    // 也可以将需要的参数写在dom上如：[data-XXX]
-    let time = $(targetEle).attr('data-time') || 2000;
-    // 最终要显示的数字
-    let finalNum = $(targetEle).attr('data-value') || 0;
-    // 调节器(毫秒数) 改变数字增加速度
-    let rate = $(targetEle).attr('data-rate') || 100;
-    // 步长
-    let step = finalNum / (time / rate);
-    // 计数器
-    let count = 0;
-    // 初始值
-    let initNum = 0;
-    let aboutChoerodonTimer = setInterval(function() {
-      count = count + step;
-      if (!count || count >= finalNum) {
-        clearInterval(aboutChoerodonTimer);
-        count = finalNum
-      }
-      // t未发生改变的话就直接返回
-      // 减少dom操作 提高DOM性能
-      let t = Math.floor(count);
-      // let t = count.toFixed(1);
-      if (t === initNum) return;
-      initNum = t;
-      $(targetEle).html(initNum);
-    }, 30)
-
-  }
-
-  function getStart() {
-    var items = $(".about-choerodon-data-item-value");
-    if (items.length) {
-      Array.from(items).forEach(function (item) {
-        numAutoPlus(item);
-      });
+function numAutoPlus(targetEle) {
+  // 动画时长(毫秒数)
+  // 也可以将需要的参数写在dom上如：[data-XXX]
+  let time = $(targetEle).attr('data-time') || 2000;
+  // 最终要显示的数字
+  let finalNum = $(targetEle).attr('data-value') || 0;
+  // 调节器(毫秒数) 改变数字增加速度
+  let rate = $(targetEle).attr('data-rate') || 100;
+  // 步长
+  let step = finalNum / (time / rate);
+  // 计数器
+  let count = 0;
+  // 初始值
+  let initNum = 0;
+  let aboutChoerodonTimer = setInterval(function() {
+    count = count + step;
+    if (!count || count >= finalNum) {
+      clearInterval(aboutChoerodonTimer);
+      count = finalNum
     }
+    // t未发生改变的话就直接返回
+    // 减少dom操作 提高DOM性能
+    let t = Math.floor(count);
+    // let t = count.toFixed(1);
+    if (t === initNum) return;
+    initNum = t;
+    $(targetEle).html(initNum);
+  }, 30)
+
+}
+
+function getStart() {
+  var items = $(".about-choerodon-data-item-value");
+  if (items.length) {
+    Array.from(items).forEach(function (item) {
+      numAutoPlus(item);
+    });
   }
-  getStart();
-});
+}
+getStart();
